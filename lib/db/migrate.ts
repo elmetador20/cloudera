4f3e2c89a9bdd16fea6e1f6f05598b280a02fbb1
sharique ahmad <ahmedsharique250@gmail.com>
2025-08-04 02:34:37 +0530

    changes

---
+++ lib/db/migrate.ts
@@ -0,0 +1,5 @@
+import { migrate } from "drizzle-orm/neon-serverless/migrator"; 
+import { drizzle } from "drizzle-orm/neon-http";
+import {neon} from "@neondatabase/serverless"
+
+import * as dotenv from "dotenv"
